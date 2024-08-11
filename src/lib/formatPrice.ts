677ffa42e0ba9b0dda0c8d399701d81e25e34867
blyncnov <bholuwatife00@gmail.com>
2024-08-11 17:42:43 +0100

    Inputs functionalities and tests

--- src/lib/formatPrice.ts
+++ src/lib/formatPrice.ts
@@ -1,4 +1,4 @@
-export function FormatPrice(price: string) {
+export function FormatPrice(price: string | number) {
   var parts = price.toString().split(".");
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   return parts.join(".");
